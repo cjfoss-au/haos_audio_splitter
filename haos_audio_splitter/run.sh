89d66ff036c730ba66d26dd8983404813270d0dc
cjfos <cjfoss.au@gmail.com>
2026-02-03 22:11:35 +1100

    Switch to matrix_spatialiser LADSPA plugin for mono conversion, bump version to 0.2.8

--- haos_audio_splitter/run.sh
+++ haos_audio_splitter/run.sh
@@ -12,8 +12,8 @@ if [ -z "$DEFAULT_SINK" ]; then
 fi
 echo "Default PulseAudio sink: $DEFAULT_SINK"
 
-su - audioaddon -c "pactl load-module module-ladspa-sink sink_name=mono_left master=$DEFAULT_SINK plugin=mono label=mono control=1,0"
-su - audioaddon -c "pactl load-module module-ladspa-sink sink_name=mono_right master=$DEFAULT_SINK plugin=mono label=mono control=0,1"
+su - audioaddon -c "pactl load-module module-ladspa-sink sink_name=mono_left master=$DEFAULT_SINK plugin=matrix_spatialiser label=matrixSpatialiser control=1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
+su - audioaddon -c "pactl load-module module-ladspa-sink sink_name=mono_right master=$DEFAULT_SINK plugin=matrix_spatialiser label=matrixSpatialiser control=0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
 
 su - audioaddon -c "PULSE_SINK=mono_left cvlc --intf telnet --telnet-password leftpass --telnet-port 4212 --aout=pulse &"
 su - audioaddon -c "PULSE_SINK=mono_right cvlc --intf telnet --telnet-password rightpass --telnet-port 4213 --aout=pulse &"
