844ef1c183ec82a0f2f9c829958adfeb070c99f1
cjfos <cjfoss.au@gmail.com>
2026-02-03 21:42:55 +1100

    Use su to run PulseAudio/VLC as non-root, bump to 0.2.2

--- haos_audio_splitter/run.sh
+++ haos_audio_splitter/run.sh
@@ -2,26 +2,22 @@
 #!/bin/bash
 
 
-# Start PulseAudio in user mode
-pulseaudio --daemonize --disallow-exit --disable-shm &
+echo "Starting PulseAudio and VLC as audioaddon user..."
+su - audioaddon -c "pulseaudio --daemonize --disallow-exit --disable-shm"
 sleep 2
 
-# Find the default sink (should be the USB device)
-DEFAULT_SINK=$(pactl list short sinks | grep -m1 -i usb | awk '{print $2}')
+DEFAULT_SINK=$(su - audioaddon -c "pactl list short sinks | grep -m1 -i usb | awk '{print \$2}'")
 if [ -z "$DEFAULT_SINK" ]; then
-  DEFAULT_SINK=$(pactl list short sinks | head -n1 | awk '{print $2}')
+  DEFAULT_SINK=$(su - audioaddon -c "pactl list short sinks | head -n1 | awk '{print \$2}'")
 fi
 echo "Default PulseAudio sink: $DEFAULT_SINK"
 
-# Create two mono sinks: left and right
-pactl load-module module-remap-sink sink_name=mono_left master=$DEFAULT_SINK channels=2 channel_map=mono,mono master_channel_map=front-left,front-right remix=no
-pactl load-module module-remap-sink sink_name=mono_right master=$DEFAULT_SINK channels=2 channel_map=mono,mono master_channel_map=front-right,front-left remix=no
+su - audioaddon -c "pactl load-module module-remap-sink sink_name=mono_left master=$DEFAULT_SINK channels=2 channel_map=mono,mono master_channel_map=front-left,front-right remix=no"
+su - audioaddon -c "pactl load-module module-remap-sink sink_name=mono_right master=$DEFAULT_SINK channels=2 channel_map=mono,mono master_channel_map=front-right,front-left remix=no"
 
-# Start two VLC instances, each bound to a different sink, listening for telnet control
-cvlc --intf telnet --telnet-password leftpass --telnet-port 4212 --aout=pulse --pulse-audio-device=mono_left &
-cvlc --intf telnet --telnet-password rightpass --telnet-port 4213 --aout=pulse --pulse-audio-device=mono_right &
+su - audioaddon -c "cvlc --intf telnet --telnet-password leftpass --telnet-port 4212 --aout=pulse --pulse-audio-device=mono_left &"
+su - audioaddon -c "cvlc --intf telnet --telnet-password rightpass --telnet-port 4213 --aout=pulse --pulse-audio-device=mono_right &"
 
 echo "Left VLC: telnet port 4212, sink mono_left. Right VLC: telnet port 4213, sink mono_right."
 
-# Keep the container running
 tail -f /dev/null
