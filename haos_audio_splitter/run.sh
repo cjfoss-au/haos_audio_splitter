b97501f6cc3b8ab5f2b686165b8a5d6ca0e633d5
cjfos <cjfoss.au@gmail.com>
2026-02-03 22:18:15 +1100

    Switch to mono LADSPA plugin for mono conversion, bump version to 0.2.13

--- haos_audio_splitter/run.sh
+++ haos_audio_splitter/run.sh
@@ -18,8 +18,8 @@ if [ -z "$DEFAULT_SINK" ]; then
 fi
 echo "Default PulseAudio sink: $DEFAULT_SINK"
 
-su - audioaddon -c "pactl load-module module-ladspa-sink sink_name=mono_left master=$DEFAULT_SINK plugin=split_1406 label=split control=1,0"
-su - audioaddon -c "pactl load-module module-ladspa-sink sink_name=mono_right master=$DEFAULT_SINK plugin=split_1406 label=split control=0,1"
+su - audioaddon -c "pactl load-module module-ladspa-sink sink_name=mono_left master=$DEFAULT_SINK plugin=mono label=mono control=1"
+su - audioaddon -c "pactl load-module module-ladspa-sink sink_name=mono_right master=$DEFAULT_SINK plugin=mono label=mono control=2"
 
 su - audioaddon -c "PULSE_SINK=mono_left cvlc --intf telnet --telnet-password leftpass --telnet-port 4212 --aout=pulse &"
 su - audioaddon -c "PULSE_SINK=mono_right cvlc --intf telnet --telnet-password rightpass --telnet-port 4213 --aout=pulse &"
