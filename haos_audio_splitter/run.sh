df88f61a52827c83a78f666ac1a527f39d278f4f
cjfos <cjfoss.au@gmail.com>
2026-02-06 21:31:20 +1100

    Remove VLC setup from run script and clean up Dockerfile, increment to 0.2.4.6

--- haos_audio_splitter/run.sh
+++ haos_audio_splitter/run.sh
@@ -14,9 +14,6 @@ pactl unload-module $(pactl list short modules | grep 'sink_name=mono_right' | a
 pactl load-module module-remap-sink sink_name=mono_left master=$DEFAULT_SINK channels=1 channel_map=mono master_channel_map=front-left remix=no
 pactl load-module module-remap-sink sink_name=mono_right master=$DEFAULT_SINK channels=1 channel_map=mono master_channel_map=front-right remix=no
 
-PULSE_SINK=mono_left cvlc --intf telnet --telnet-password leftpass --telnet-port 4212 --aout=pulse &
-PULSE_SINK=mono_right cvlc --intf telnet --telnet-password rightpass --telnet-port 4213 --aout=pulse &
-
-echo "Left VLC: telnet port 4212, sink mono_left. Right VLC: telnet port 4213, sink mono_right."
+echo "Created mono_left and mono_right sinks."
 
 tail -f /dev/null
